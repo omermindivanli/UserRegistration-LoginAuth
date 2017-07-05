c6656210562fbf930c624c92bd4fd219421d4158
Ömer Mindivanli <omer.mindivanli@gmail.com>
2017-07-05 15:20:19 +0300

    Wrong connection script. That is enough
    There was an extra connection scripts. A person does not have to add two different $connection variables for login to a database.

--- config.php
+++ config.php
@@ -3,14 +3,3 @@
 	$connection = mysqli_connect('localhost', 'root', '123456','cc');
 
 ?>
- /*
-
-<?php 
-  define('DB_NAME', $_SERVER['RDS_DB_NAME']);
-  define('DB_USER', $_SERVER['root']);
-  define('DB_PASSWORD', $_SERVER['']);
-  define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
-  define('DB_TABLE', 'urler');
-?>
-
- */
